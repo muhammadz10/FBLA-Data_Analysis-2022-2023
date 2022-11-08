79be7c44fddfaa96319ec44665ccb8e404bd9268
firebolt2000herry <lvedant54@gmail.com>
2022-11-07 20:50:00 -0800

    11/7/22 8:50pm

---
+++ main.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+df = pd.read_csv(r'"C:\Users\lveda\OneDrive\Desktop\Datssets.csv\GDP (current US$).xls - Data (1).csv"')
+print(df)
